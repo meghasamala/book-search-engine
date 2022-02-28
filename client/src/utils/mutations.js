32b23a2174de6e9af0e7261830813f074cb729fe
megha <msamala21@gmail.com>
2022-02-27 19:42:33 -0600

    fix files for heroku deployment

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,54 @@
+import gql from 'graphql-tag';
+
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($authors: [String]!, $description: String!, $title: String!, $bookId: String!, $image: String!){
+    saveBook(authors: $authors, description: $description, title: $title, bookId: $bookId, image: $image) {
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+      }
+    }
+  }
+`;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,37 @@
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
+const { gql } = require('apollo-server-express');
 
-module.exports = { typeDefs, resolvers };
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(authors: [String]!, description: String!, title: String!, bookId: String!, image: String!): User
+    removeBook(bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
